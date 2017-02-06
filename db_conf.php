89e0da64e5366a8dcd405702d6b08aa86744b69c
Oliver <oliver.otsmann@khk.ee>
2017-02-06 15:01:21 +0200

    Add files via upload

---
+++ db_conf.php
@@ -0,0 +1,27 @@
+<?php
+
+// define constants
+
+define('DBHOST', '138.68.68.130');
+define('DBUSER', 'oliverotsmann');
+define('DBPASS', 'oov6iegh');
+define('DBNAME', 'oliverotsmann_db');
+
+/*
+// import classes
+require_once CLASSES_DIR.'template.php'; // import template class file
+require_once CLASSES_DIR.'http.php'; // import http class file
+require_once CLASSES_DIR.'linkobject.php'; // import linkobject file
+require_once CLASSES_DIR.'mysql.php'; // import database class file
+require_once CLASSES_DIR.'session.php'; // import session class file
+// import database configuration
+require_once 'db_conf.php';
+// objects
+// create linkobject object, because it extends http object
+$http = new linkobject();
+// create database class object with real values
+$db = new mysql('138.68.68.130', 'oliverotsmann' , 'oov6iegh', 'oliverotsmann_db');
+// create session class object
+$sess = new session($http, $db);
+*/
+?>
